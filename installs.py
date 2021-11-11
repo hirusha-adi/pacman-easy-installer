cf7ff9ab74e65ac5748ae0a20b677543d6dbae10
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2021-11-11 20:42:05 +0530

    nice

--- installs.py
+++ installs.py
@@ -17,9 +17,14 @@ def pacman_install(package_name: str = None):
     os.system(f"pacman -S {package_name.lower()}")
 
 
-def install_for_me_custom():
+def install_all_for_mainrig():
+    """
+    Should i run these? or should i keep the zen kernel
+        sudo pacman -S linux-lts
+        sudo pacman -S linux-lts-headers
+    """
     all_packages = [
-        "atom", "audacious", "audacity", "bitwarden", "discord",
+        "base-devel", "git", "atom", "audacious", "audacity",
         "eclipse-java", "firefox", "flameshot", "gimp", "kate",
         "github-desktop",  "kdenlive", "nano", "vim", "neofetch",
         "netbeans", "nmap", "obs-studio", "pamac-aur", "qbittorrent",
@@ -27,51 +32,9 @@ def install_for_me_custom():
         "sublime-text-4", "steam", "sudo", "teams", "teamviewer",
         "telegram-desktop", "tor-browser", "tk", "vlc", "wget",
         "wps-office", "woeusb-gui", "yakuake", "yay", "zoom",
-        "bpytop", "htop", "ffpmeg"
-    ]
-    for package in all_packages:
-        pacman_install(package_name=package)
-
-
-def install_media_client_side():
-    all_packages = [
-        "audacious", "audacity", "obs-studio", "kdenlive",
-        "qbittorrent", "ffmpeg"
-    ]
-    for package in all_packages:
-        pacman_install(package_name=package)
-
-
-def install_media_server_side():
-    all_packages = [
-        "jellyfin", "radarr", "lidarr", "sonarr",
-        "qbittorrent", "ffmpeg"
-    ]
-    for package in all_packages:
-        pacman_install(package_name=package)
-
-
-def install_developer():
-    all_packages = [
-        "atom", "eclipse-java", "netbeans", "nano", "vim", "kate",
-        "pycharm-community-edition", "python3-pip", "sublime-text-4",
-        "tk", "yakuake", "python3", "github-desktop"
-    ]
-    for package in all_packages:
-        pacman_install(package_name=package)
-
-
-def install_general_purpose():
-    all_packages = [
-        "audacious", "audacity", "bitwarden", "discord",
-        "firefox", "flameshot", "gimp", "kate",
-        "kdenlive", "nano", "neofetch",
-        "obs-studio", "pamac-aur", "qbittorrent",
-        "snapd", "spotify-adblock-git",
-        "steam", "sudo", "teams", "teamviewer",
-        "telegram-desktop", "tor-browser", "tk", "vlc",
-        "wps-office", "woeusb-gui", "yay", "zoom",
-        "htop", "ffpmeg"
+        "bpytop", "htop", "ffpmeg", "partitionmanager", "curl",
+        "discord", "bitwarden", "gedit",
+        "p7zip", "p7zip-plugins", "unrar", "tar", "rsync"
     ]
     for package in all_packages:
         pacman_install(package_name=package)
@@ -91,38 +54,36 @@ def ENTIRE_PROGRAM():
             * Machine: {platform.machine()}
     """)
     print(""" 
-             [1] General Purpose
-             [2] Media Interested User
-             [3] Media Server
-             [4] For Developers
-             [5] Upgrade Everything
-             [99] Custom for @hirusha-adi  
+             [1] MainRig
     """)
     mmo = input("? Please select and option: ")
     if mmo == "1":
         os.system("clear")
         print("""
-                                     _                  
-      __ _  ___ _ __   ___ _ __ __ _| |  _   _ ___  ___ 
-     / _` |/ _ \ '_ \ / _ \ '__/ _` | | | | | / __|/ _ \\
-    | (_| |  __/ | | |  __/ | | (_| | | | |_| \__ \  __/
-     \__, |\___|_| |_|\___|_|  \__,_|_|  \__,_|___/\___|
-     |___/                                              
+  _     _                _             ____  __  __       _       ____  _       
+ | |__ (_)_ __ _   _ ___| |__   __ _  / __ \|  \/  | __ _(_)_ __ |  _ \(_) __ _ 
+ | '_ \| | '__| | | / __| '_ \ / _` |/ / _` | |\/| |/ _` | | '_ \| |_) | |/ _` |
+ | | | | | |  | |_| \__ \ | | | (_| | | (_| | |  | | (_| | | | | |  _ <| | (_| |
+ |_| |_|_|_|   \__,_|___/_| |_|\__,_|\ \__,_|_|  |_|\__,_|_|_| |_|_| \_\_|\__, |
+                                      \____/                              |___/ 
     
      + You will be installing the packages listed below
         """)
-        print("""            audacity, bitwarden, htop, ffpmeg,zoom,
-            discord, firefox, flameshot, gimp, kate,
-            kdenlive, nano, neofetch, obs-studio, 
-            pamac-aur, qbittorrent, snapd, steam,
-            spotify-adblock-git,sudo, teams,  vlc,
-            teamviewer,telegram-desktop", woeusb-gui,
-            tor-browser, tk, wps-office, yay
+        print("""        atom, audacious, audacity, bitwarden, discord,
+        eclipse-java, firefox, flameshot, gimp, kate
+        github-desktop, kdenlive, nano, vim, neofetch
+        netbeans, nmap, obs-studio,amacaur, qbittorrent
+        pycharm-community-edition, python-pip, snapd, 
+        spotify-dblockgit, partitionmanager, ffpmeg, 
+        sublime-text-4,steam, sudo, teams, teamviewer
+        telegram-desktop, tor-browser, tk, vlc, wget
+        wps-office, woeusb-gui, yakuake, yay, zoom
+        bpytop, htop, curl
              """)
         smo1 = input("? Hit enter if you want to continue: ")
         if smo1.lower().startswith("q"):
             exit()
-        install_general_purpose()
+        install_all_for_mainrig()
 
 
 if __name__ == "__main__":
